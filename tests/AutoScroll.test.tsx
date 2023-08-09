ccb272dcf2c82ec9ebd1849cc851560a53e0bd72
amochkin <amochkin@gmail.com>
2023-08-09 13:21:11 +0200

    fix: refactored tests to make it work

--- tests/AutoScroll.test.tsx
+++ tests/AutoScroll.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'react-native';
+import { describe, expect, it, test } from '@jest/globals';
 /* Note: test renderer must be required after react-native */
 import * as renderer from 'react-test-renderer';
 import AutoScroll from '../src';
